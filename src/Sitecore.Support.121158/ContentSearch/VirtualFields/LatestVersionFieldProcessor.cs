821a48186e225365c17e44fa676bc1da47689bd6
DK\psk <psk@sitecore.net>
2018-08-24 14:44:00 +0300

    Fix the issue

--- src/Sitecore.Support.121158/ContentSearch/VirtualFields/LatestVersionFieldProcessor.cs
+++ src/Sitecore.Support.121158/ContentSearch/VirtualFields/LatestVersionFieldProcessor.cs
@@ -1,6 +1,32 @@
 ﻿namespace Sitecore.Support.ContentSearch.VirtualFields
 {
-  public class LatestVersionFieldProcessor : Sitecore.ContentSearch.VirtualFields.LatestVersionFieldProcessor, Sitecore.ContentSearch.IVirtualFieldProcessor
+  using System.Collections.Generic;
+  using Sitecore.ContentSearch;
+  using Sitecore.ContentSearch.Linq.Common;
+  using Sitecore.ContentSearch.Pipelines.ProcessFacets;
+  [UsedImplicitly]
+  public class LatestVersionFieldProcessor : Sitecore.ContentSearch.VirtualFields.LatestVersionFieldProcessor, IVirtualFieldProcessor, IFieldQueryTranslator
   {
+    IDictionary<string, object> IFieldQueryTranslator.TranslateFieldResult(IDictionary<string, object> fields, FieldNameTranslator fieldNameTranslator)
+    {
+      var fieldValue = fields[fieldNameTranslator.GetIndexFieldName(BuiltinFields.LatestVersion)];
+      fields.Add(this.FieldName, fieldValue);
+      return fields;
+    }
+
+    IDictionary<string, ICollection<KeyValuePair<string, int>>> IVirtualFieldProcessor.TranslateFacetResult(ProcessFacetsArgs args)
+    {
+      var facets = args.Facets;
+
+      ICollection<KeyValuePair<string, int>> versionFacet;
+      var fieldName = args.FieldNameTranslator.GetIndexFieldName(this.FieldName);
+      if (facets.TryGetValue(fieldName, out versionFacet))
+      {
+        facets.Remove(fieldName);
+        facets[BuiltinFields.LatestVersion] = versionFacet;
+      }
+
+      return facets;
+    }
   }
 }
--- src/Sitecore.Support.121158/ContentSearch/VirtualFields/UniqueIdFieldProcessor.cs
+++ src/Sitecore.Support.121158/ContentSearch/VirtualFields/UniqueIdFieldProcessor.cs
@@ -1,6 +1,32 @@
 ﻿namespace Sitecore.Support.ContentSearch.VirtualFields
 {
-  public class UniqueIdFieldProcessor : Sitecore.ContentSearch.VirtualFields.UniqueIdFieldProcessor, Sitecore.ContentSearch.IVirtualFieldProcessor
+  using System.Collections.Generic;
+  using Sitecore.ContentSearch;
+  using Sitecore.ContentSearch.Linq.Common;
+  using Sitecore.ContentSearch.Pipelines.ProcessFacets;
+  [UsedImplicitly]
+  public class UniqueIdFieldProcessor : Sitecore.ContentSearch.VirtualFields.UniqueIdFieldProcessor, IVirtualFieldProcessor, IFieldQueryTranslator
   {
+    IDictionary<string, object> IFieldQueryTranslator.TranslateFieldResult(IDictionary<string, object> fields, FieldNameTranslator fieldNameTranslator)
+    {
+      var fieldValue = fields[fieldNameTranslator.GetIndexFieldName(BuiltinFields.UniqueId)];
+      fields.Add(this.FieldName, fieldValue);
+      return fields;
+    }
+
+    IDictionary<string, ICollection<KeyValuePair<string, int>>> IVirtualFieldProcessor.TranslateFacetResult(ProcessFacetsArgs args)
+    {
+      var facets = args.Facets;
+
+      ICollection<KeyValuePair<string, int>> versionFacet;
+      var fieldName = args.FieldNameTranslator.GetIndexFieldName(this.FieldName);
+      if (facets.TryGetValue(fieldName, out versionFacet))
+      {
+        facets.Remove(fieldName);
+        facets[BuiltinFields.UniqueId] = versionFacet;
+      }
+
+      return facets;
+    }
   }
 }
